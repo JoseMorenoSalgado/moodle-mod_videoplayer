512bbda78470e229b24c6e2997f51b1021835ce6
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2025-07-15 12:39:57 -0600

    Add files via upload

---
+++ videoplayer/lang/en/videoplayer.php
@@ -0,0 +1,7 @@
+<?php
+$string['pluginname'] = 'Video Elearning Cloud';
+$string['modulename'] = 'Video Elearning Cloud';
+$string['modulenameplural'] = 'Videos Elearning Cloud';
+$string['videoname'] = 'Título del video';
+$string['videourl'] = 'Enlace de Google Drive';
+$string['invalidurl'] = 'URL de Google Drive no válida. Asegúrate de usar el formato correcto.';
