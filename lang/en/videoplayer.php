f3cf7887be392b64fc3a0da670039240ab340b02
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2025-07-19 12:45:59 -0600

    Update videoplayer.php

--- lang/en/videoplayer.php
+++ lang/en/videoplayer.php
@@ -1,6 +1,42 @@
 <?php
-// Strings for the videoplayer module capabilities.
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+
+/**
+ * Version information for the Video Player activity module.
+ *
+ * @package    mod_videoplayer
+ * @copyright  2025 Jose Erasmo Moreno Salgado - Elearning Cloud <jose@elearningcloud.org>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+// General strings.
+$string['modulename'] = 'Video player';
+$string['modulenameplural'] = 'Video players';
+$string['modulename_help'] = 'Use this module to embed and display videos from Google Drive or other sources.';
+$string['pluginname'] = 'Video player';
+
+// Form strings.
+$string['videoname'] = 'Video title';
+$string['videourl'] = 'Google Drive video URL';
+$string['videourl_help'] = 'Paste the shareable link from Google Drive. The video will be embedded in the activity.';
+
+// Capability strings.
 $string['mod_videoplayer:addinstance'] = 'Add a new Video Player instance';
 $string['mod_videoplayer:addinstance_help'] = 'Allows users to add a new instance of the Video Player activity to a course.';
 
