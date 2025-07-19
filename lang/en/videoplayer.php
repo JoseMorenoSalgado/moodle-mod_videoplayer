75c79b3c050f53f71b6ffcb79bb56c0ec378249e
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2025-07-19 12:13:02 -0600

    Update videoplayer.php

--- lang/en/videoplayer.php
+++ lang/en/videoplayer.php
@@ -1,7 +1,20 @@
 <?php
-$string['pluginname'] = 'Video Elearning Cloud';
-$string['modulename'] = 'Video Elearning Cloud';
-$string['modulenameplural'] = 'Videos Elearning Cloud';
-$string['videoname'] = 'Título del video';
-$string['videourl'] = 'Enlace de Google Drive';
-$string['invalidurl'] = 'URL de Google Drive no válida. Asegúrate de usar el formato correcto.';
+// Strings for the videoplayer module capabilities.
+
+$string['mod_videoplayer:addinstance'] = 'Add a new Video Player instance';
+$string['mod_videoplayer:addinstance_help'] = 'Allows users to add a new instance of the Video Player activity to a course.';
+
+$string['mod_videoplayer:view'] = 'View Video Player activity';
+$string['mod_videoplayer:view_help'] = 'Allows users to view the Video Player activity content.';
+
+$string['mod_videoplayer:edit'] = 'Edit Video Player activity';
+$string['mod_videoplayer:edit_help'] = 'Allows users to edit the settings or content of the Video Player activity.';
+
+$string['mod_videoplayer:manage'] = 'Manage Video Player activity';
+$string['mod_videoplayer:manage_help'] = 'Allows users to manage general module configuration for the Video Player activity.';
+
+$string['mod_videoplayer:viewreport'] = 'View Video Player reports';
+$string['mod_videoplayer:viewreport_help'] = 'Allows users to view reports related to the Video Player activity.';
+
+$string['mod_videoplayer:editreport'] = 'Edit Video Player reports';
+$string['mod_videoplayer:editreport_help'] = 'Allows users to edit reports, such as user progress, in the Video Player activity.';
