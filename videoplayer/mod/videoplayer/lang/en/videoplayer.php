924811c8ee53fd1e5445c55f696deab20870514a
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2025-07-19 11:04:35 -0600

    Delete videoplayer directory

--- videoplayer/mod/videoplayer/lang/en/videoplayer.php
+++
@@ -1,7 +0,0 @@
-<?php
-$string['pluginname'] = 'Video Elearning Cloud';
-$string['modulename'] = 'Video Elearning Cloud';
-$string['modulenameplural'] = 'Videos Elearning Cloud';
-$string['videoname'] = 'Título del video';
-$string['videourl'] = 'Enlace de Google Drive';
-$string['invalidurl'] = 'URL de Google Drive no válida. Asegúrate de usar el formato correcto.';
--- videoplayer/version.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('MOODLE_INTERNAL') || die();
-
-$plugin->component = 'mod_videoplayer';
-$plugin->version = 2025071500;
-$plugin->requires = 2022041900; // Moodle 4.1
-$plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.0';
