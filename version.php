c99342ddf5290c62f6d56af63bbf27f9ecc108b0
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2025-07-19 11:34:53 -0600

    Update version.php

--- version.php
+++ version.php
@@ -1,8 +1,31 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ * View page for the Video Player activity.
+ *
+ * @package    mod_videoplayer
+ * @copyright  2025 Jose Erasmo Moreno Salgado - Elearning Cloud  <jose@elearningcloud.org>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->component = 'mod_videoplayer';
-$plugin->version = 2025071500;
-$plugin->requires = 2022041900; // Moodle 4.1
-$plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.0';
+$plugin->component = 'mod_videoplayer';        // Full name of the plugin (used for diagnostics).
+$plugin->release   = '1.0.0';                  // Human-readable version name.
+$plugin->version   = 2025071900;               // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires  = 2022041900;               // Moodle version required (e.g., 4.0 = 2022041900).
+$plugin->supported = [40100, 50000];           // Supported Moodle versions (e.g., 4.1 to 5.0).
+$plugin->maturity  = MATURITY_STABLE;          // Maturity level of the plugin (e.g., ALPHA, BETA, RC, STABLE).
