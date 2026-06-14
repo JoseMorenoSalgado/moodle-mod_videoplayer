0311d6996012de0fad091ec8fc946c1e5bf857f6
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-06-14 13:36:26 -0600

    Complete Drive Resource English strings

--- lang/en/videoplayer.php
+++ lang/en/videoplayer.php
@@ -11,11 +11,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$string['pluginname'] = 'Google Drive resource';
-$string['modulename'] = 'Google Drive resource';
-$string['modulenameplural'] = 'Google Drive resources';
+$string['pluginname'] = 'Drive Resource';
+$string['modulename'] = 'Drive Resource';
+$string['modulenameplural'] = 'Drive Resources';
 $string['modulename_help'] = 'Use this activity to embed videos, PDFs, images, documents, spreadsheets and presentations from Google Drive.';
-$string['pluginadministration'] = 'Google Drive resource administration';
+$string['pluginadministration'] = 'Drive Resource administration';
 
 $string['resourcename'] = 'Resource name';
 $string['videoname'] = 'Resource name';
@@ -27,6 +27,13 @@ $string['resourcetype'] = 'Resource type';
 $string['completionpercentage'] = 'Required completion percentage';
 $string['completionpercentage_help'] = 'Percentage required to consider this resource completed when progress tracking is available. For non-video resources, Moodle view completion may be used.';
 $string['openindrive'] = 'Open in Google Drive';
+$string['protectedresource'] = 'Protected resource';
+$string['backtoresource'] = 'Back to resource';
+$string['progress'] = 'Progress';
+$string['progressreport'] = 'Progress report';
+$string['noprogressrecords'] = 'There are no progress records for this resource yet.';
+$string['noresources'] = 'There are no Drive Resources in this course.';
+$string['noresourcesavailable'] = 'There are no Drive Resources available to you in this course.';
 
 $string['typeauto'] = 'Automatic';
 $string['typevideo'] = 'Video';
@@ -41,21 +48,32 @@ $string['invalidurl'] = 'The provided URL is not valid. Please use a proper Goog
 $string['invaliddriveurl'] = 'Enter a valid Google Drive or Google Docs shareable URL.';
 $string['invalidcompletionpercentage'] = 'The completion percentage must be a number between 0 and 100.';
 
-$string['mod_videoplayer:addinstance'] = 'Add a new Google Drive resource';
-$string['mod_videoplayer:addinstance_help'] = 'Allows users to add a new Google Drive resource activity to a course.';
-$string['mod_videoplayer:view'] = 'View Google Drive resource';
-$string['mod_videoplayer:view_help'] = 'Allows users to view the Google Drive resource activity content.';
-$string['mod_videoplayer:edit'] = 'Edit Google Drive resource';
-$string['mod_videoplayer:edit_help'] = 'Allows users to edit the Google Drive resource settings.';
-$string['mod_videoplayer:manage'] = 'Manage Google Drive resource';
-$string['mod_videoplayer:manage_help'] = 'Allows users to manage Google Drive resource configuration.';
-$string['mod_videoplayer:viewreport'] = 'View Google Drive resource reports';
-$string['mod_videoplayer:viewreport_help'] = 'Allows users to view reports related to Google Drive resources.';
-$string['mod_videoplayer:editreport'] = 'Edit Google Drive resource reports';
-$string['mod_videoplayer:editreport_help'] = 'Allows users to edit reports and user progress in Google Drive resources.';
+$string['setting_enabletracking'] = 'Enable progress tracking';
+$string['setting_enabletracking_desc'] = 'When enabled, Drive Resource records user presence and interaction time in the activity.';
+$string['setting_defaultrequiredseconds'] = 'Default required time';
+$string['setting_defaultrequiredseconds_desc'] = 'Default active time in seconds required to consider a resource sufficiently viewed when presence-based tracking is used.';
+$string['setting_defaultcompletionpercentage'] = 'Default completion percentage';
+$string['setting_defaultcompletionpercentage_desc'] = 'Default percentage used when creating new Drive Resource activities.';
+$string['setting_protectedmode'] = 'Protected mode';
+$string['setting_protectedmode_desc'] = 'When enabled, Drive Resource hides direct Google Drive navigation links and limits iframe popup permissions. Google Drive internal controls may still be controlled by Google.';
+$string['setting_showresourcetype'] = 'Show resource type';
+$string['setting_showresourcetype_desc'] = 'Show the detected or selected resource type above the embedded resource.';
 
-$string['privacy:metadata:videoplayer_views'] = 'Stores user progress and completion data for Google Drive resources.';
-$string['privacy:metadata:videoplayer_views:videoplayerid'] = 'The Google Drive resource activity instance ID.';
+$string['mod_videoplayer:addinstance'] = 'Add a new Drive Resource';
+$string['mod_videoplayer:addinstance_help'] = 'Allows users to add a new Drive Resource activity to a course.';
+$string['mod_videoplayer:view'] = 'View Drive Resource';
+$string['mod_videoplayer:view_help'] = 'Allows users to view the Drive Resource activity content.';
+$string['mod_videoplayer:edit'] = 'Edit Drive Resource';
+$string['mod_videoplayer:edit_help'] = 'Allows users to edit the Drive Resource settings.';
+$string['mod_videoplayer:manage'] = 'Manage Drive Resource';
+$string['mod_videoplayer:manage_help'] = 'Allows users to manage Drive Resource configuration.';
+$string['mod_videoplayer:viewreport'] = 'View Drive Resource reports';
+$string['mod_videoplayer:viewreport_help'] = 'Allows users to view reports related to Drive Resources.';
+$string['mod_videoplayer:editreport'] = 'Edit Drive Resource reports';
+$string['mod_videoplayer:editreport_help'] = 'Allows users to edit reports and user progress in Drive Resources.';
+
+$string['privacy:metadata:videoplayer_views'] = 'Stores user progress and completion data for Drive Resources.';
+$string['privacy:metadata:videoplayer_views:videoplayerid'] = 'The Drive Resource activity instance ID.';
 $string['privacy:metadata:videoplayer_views:userid'] = 'The ID of the user who viewed the resource.';
 $string['privacy:metadata:videoplayer_views:progress'] = 'The last saved progress value.';
 $string['privacy:metadata:videoplayer_views:completed'] = 'Whether the resource has been marked as completed.';
