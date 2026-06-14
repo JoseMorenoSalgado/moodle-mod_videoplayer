a7800b2d076029e1e2eba3bf86ea7e87901cfcdc
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-06-14 11:39:35 -0600

    Refactor restore structure

--- backup/moodle2/restore_videoplayer_stepslib.php
+++ backup/moodle2/restore_videoplayer_stepslib.php
@@ -1,49 +1,91 @@
 <?php
 // This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
  * Restore steps for mod_videoplayer.
  *
- * @package   mod_videoplayer
- * @category  backup
- * @copyright 2025 Jose Erasmo Moreno Salgado
- * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package    mod_videoplayer
+ * @category   backup
+ * @copyright  2026 Jose Erasmo Moreno Salgado - Elearning Cloud
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
+/**
+ * Restore structure step for the videoplayer activity.
+ */
 class restore_videoplayer_activity_structure_step extends restore_activity_structure_step {
 
+    /**
+     * Define restore paths.
+     *
+     * @return array
+     */
     protected function define_structure() {
         $paths = [];
+        $userinfo = $this->get_setting_value('userinfo');
 
         $paths[] = new restore_path_element('videoplayer', '/activity/videoplayer');
+
+        if ($userinfo) {
+            $paths[] = new restore_path_element('videoplayer_view', '/activity/videoplayer/views/view');
+        }
 
         return $this->prepare_activity_structure($paths);
     }
 
+    /**
+     * Restore the activity instance.
+     *
+     * @param array|stdClass $data
+     */
     protected function process_videoplayer($data) {
         global $DB;
 
-        $data = (object)$data;
+        $data = (object) $data;
+        $oldid = $data->id;
 
         $data->course = $this->get_courseid();
 
-        $newitemid = $DB->insert_record('videoplayer', $data);
+        if (empty($data->source)) {
+            $data->source = 'googledrive';
+        }
+        if (empty($data->type)) {
+            $data->type = 'auto';
+        }
+        if (!isset($data->completionpercentage) || $data->completionpercentage === '') {
+            $data->completionpercentage = 80;
+        }
 
+        $newitemid = $DB->insert_record('videoplayer', $data);
+        $this->set_mapping('videoplayer', $oldid, $newitemid);
         $this->apply_activity_instance($newitemid);
+    }
+
+    /**
+     * Restore user view/progress records.
+     *
+     * @param array|stdClass $data
+     */
+    protected function process_videoplayer_view($data) {
+        global $DB;
+
+        $data = (object) $data;
+        $data->videoplayerid = $this->get_new_parentid('videoplayer');
+        $data->userid = $this->get_mappingid('user', $data->userid);
+
+        if (empty($data->userid)) {
+            return;
+        }
+
+        $DB->insert_record('videoplayer_views', $data);
+    }
+
+    /**
+     * Run after restore execution.
+     */
+    protected function after_execute() {
+        // No file areas currently used by this activity.
     }
 }
