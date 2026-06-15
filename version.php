86ccc1181bcb65c804c14527c35a922f0c9c7c5f
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-06-15 17:01:55 -0600

    Bump version for PDF first-load tuning

--- version.php
+++ version.php
@@ -3,7 +3,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin = new stdClass();
 $plugin->component = 'mod_videoplayer';
-$plugin->version = 2026061508;
-$plugin->release = '1.0.8';
+$plugin->version = 2026061509;
+$plugin->release = '1.0.9';
 $plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_BETA;
