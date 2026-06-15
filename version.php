8df3a0d1b8609bafc4c3dd75f262b0a8cc521596
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-06-15 14:05:53 -0600

    Simplify version metadata for Moodle 5.1 compatibility

--- version.php
+++ version.php
@@ -18,8 +18,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin = new stdClass();
 $plugin->component = 'mod_videoplayer';
-$plugin->version = 2026061501;
-$plugin->release = '1.0.1';
-$plugin->requires = 2024100700;
-$plugin->supported = [40500, 50100];
+$plugin->version = 2026061502;
+$plugin->release = '1.0.2';
+$plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_BETA;
