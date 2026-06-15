691a4aff76cdd4962d479fc4d79882f61aef53b7
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-06-15 16:25:33 -0600

    Bump version for PDF UI refinement

--- version.php
+++ version.php
@@ -3,7 +3,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin = new stdClass();
 $plugin->component = 'mod_videoplayer';
-$plugin->version = 2026061506;
-$plugin->release = '1.0.6';
+$plugin->version = 2026061507;
+$plugin->release = '1.0.7';
 $plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_BETA;
