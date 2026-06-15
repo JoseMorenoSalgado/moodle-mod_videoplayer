4bc9a828dca3f43ccaead0e2d1729ca00dc6cbb2
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-06-15 16:03:03 -0600

    Bump version for PDF cache support

--- version.php
+++ version.php
@@ -1,16 +1,9 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 
-/**
- * Version information for Drive Resource.
- *
- * @package    mod_videoplayer
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
-
 $plugin = new stdClass();
 $plugin->component = 'mod_videoplayer';
-$plugin->version = 2026061504;
-$plugin->release = '1.0.4';
+$plugin->version = 2026061506;
+$plugin->release = '1.0.6';
 $plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_BETA;
