f9b675d419b96cb7dd7c428247eae4dd927b756d
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-06-15 17:09:05 -0600

    Bump version for shared PDF cache cleanup

--- version.php
+++ version.php
@@ -3,7 +3,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin = new stdClass();
 $plugin->component = 'mod_videoplayer';
-$plugin->version = 2026061509;
-$plugin->release = '1.0.9';
+$plugin->version = 2026061510;
+$plugin->release = '1.0.10';
 $plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_BETA;
