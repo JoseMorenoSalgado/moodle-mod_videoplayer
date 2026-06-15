ba3a026830980a57202a896c08dfc6ed9f85cccd
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-06-15 17:26:33 -0600

    Bump version for 30 day PDF cache default

--- version.php
+++ version.php
@@ -3,7 +3,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin = new stdClass();
 $plugin->component = 'mod_videoplayer';
-$plugin->version = 2026061510;
-$plugin->release = '1.0.10';
+$plugin->version = 2026061511;
+$plugin->release = '1.0.11';
 $plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_BETA;
