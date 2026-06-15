fec8862591dd45662396718e87ab321edf7288b8
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-06-15 15:16:34 -0600

    Bump version after protected proxy fixes

--- version.php
+++ version.php
@@ -1,24 +1,16 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-
 defined('MOODLE_INTERNAL') || die();
 
 /**
  * Version information for Drive Resource.
  *
  * @package    mod_videoplayer
- * @copyright  2026 Jose Erasmo Moreno Salgado - Elearning Cloud <jose@elearningcloud.org>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 $plugin = new stdClass();
 $plugin->component = 'mod_videoplayer';
-$plugin->version = 2026061502;
-$plugin->release = '1.0.2';
+$plugin->version = 2026061503;
+$plugin->release = '1.0.3';
 $plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_BETA;
