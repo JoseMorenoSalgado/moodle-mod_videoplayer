969877602e3e1365e25943655a12f4ddeb9ea567
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-06-15 23:16:42 -0600

    Add local Plyr AMD initializer

---
+++ amd/src/plyr.js
@@ -0,0 +1,77 @@
+define(['core/notification'], function(Notification) {
+    var PLYR_URL = M.cfg.wwwroot + '/mod/videoplayer/thirdpartylibs/plyr/plyr.min.js';
+    var plyrPromise = null;
+
+    var loadPlyr = function() {
+        if (window.Plyr) {
+            return Promise.resolve(window.Plyr);
+        }
+
+        if (!plyrPromise) {
+            plyrPromise = new Promise(function(resolve, reject) {
+                var script = document.createElement('script');
+                script.src = PLYR_URL;
+                script.async = true;
+                script.onload = function() {
+                    if (window.Plyr) {
+                        resolve(window.Plyr);
+                    } else {
+                        reject(new Error('Plyr did not expose window.Plyr'));
+                    }
+                };
+                script.onerror = function() {
+                    reject(new Error('Local Plyr library could not be loaded'));
+                };
+                document.head.appendChild(script);
+            });
+        }
+
+        return plyrPromise;
+    };
+
+    var init = function() {
+        var nodes = Array.prototype.slice.call(document.querySelectorAll('.js-drive-resource-video'));
+        if (!nodes.length) {
+            return;
+        }
+
+        loadPlyr().then(function(Plyr) {
+            nodes.forEach(function(node) {
+                if (node.dataset.plyrReady === '1') {
+                    return;
+                }
+
+                node.dataset.plyrReady = '1';
+                new Plyr(node, {
+                    controls: ['play-large', 'play', 'progress', 'current-time', 'duration', 'mute', 'volume', 'settings', 'fullscreen'],
+                    settings: ['speed'],
+                    speed: {
+                        selected: 1,
+                        options: [0.5, 0.75, 1, 1.25, 1.5, 2]
+                    },
+                    hideControls: true,
+                    keyboard: {
+                        focused: true,
+                        global: false
+                    },
+                    tooltips: {
+                        controls: true,
+                        seek: true
+                    }
+                });
+            });
+        }).catch(function(error) {
+            if (window.console) {
+                window.console.warn(error.message || error);
+            }
+            Notification.addNotification({
+                message: M.util.get_string('plyrmissing', 'mod_videoplayer'),
+                type: 'warning'
+            });
+        });
+    };
+
+    return {
+        init: init
+    };
+});
