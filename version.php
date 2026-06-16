20a140548566bfa21ccabb0c28a565ce2b37f760
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-06-16 08:58:27 -0600

    Bump version for configurable Plyr player colors

--- version.php
+++ version.php
@@ -3,7 +3,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin = new stdClass();
 $plugin->component = 'mod_videoplayer';
-$plugin->version = 2026061516;
-$plugin->release = '1.0.16';
+$plugin->version = 2026061517;
+$plugin->release = '1.0.17';
 $plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_BETA;
