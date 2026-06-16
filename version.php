76a69c1266f582913964dda4fd798ec3f42857f9
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-06-15 22:29:28 -0600

    Bump version for local Video.js integration

--- version.php
+++ version.php
@@ -3,7 +3,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin = new stdClass();
 $plugin->component = 'mod_videoplayer';
-$plugin->version = 2026061513;
-$plugin->release = '1.0.13';
+$plugin->version = 2026061514;
+$plugin->release = '1.0.14';
 $plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_BETA;
