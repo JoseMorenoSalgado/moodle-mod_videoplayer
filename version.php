036a5ce9b6616dd7d936571d238acca26d32b81f
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-06-16 07:12:32 -0600

    Bump version for complete Plyr replacement

--- version.php
+++ version.php
@@ -3,7 +3,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin = new stdClass();
 $plugin->component = 'mod_videoplayer';
-$plugin->version = 2026061515;
-$plugin->release = '1.0.15';
+$plugin->version = 2026061516;
+$plugin->release = '1.0.16';
 $plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_BETA;
