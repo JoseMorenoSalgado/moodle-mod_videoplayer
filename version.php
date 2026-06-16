1ccdeca23f96c3b710764232e99b7047f62a08f1
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-06-15 22:08:23 -0600

    Bump version for native video player

--- version.php
+++ version.php
@@ -3,7 +3,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin = new stdClass();
 $plugin->component = 'mod_videoplayer';
-$plugin->version = 2026061511;
-$plugin->release = '1.0.11';
+$plugin->version = 2026061513;
+$plugin->release = '1.0.13';
 $plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_BETA;
