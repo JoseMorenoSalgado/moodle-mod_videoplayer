89d53a9c021836410d952b83442792965a865ad0
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-06-22 07:25:05 -0600

    Update release number

--- version.php
+++ version.php
@@ -3,7 +3,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin = new stdClass();
 $plugin->component = 'mod_videoplayer';
-$plugin->version = 2026062202;
-$plugin->release = '1.1.3-beta';
+$plugin->version = 2026062203;
+$plugin->release = '1.1.4-beta';
 $plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_BETA;
