643889e51f2daa1e0a4098c505c690b671d1901d
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-06-22 09:10:30 -0600

    Bump release for responsive PDF.js viewer

--- version.php
+++ version.php
@@ -3,7 +3,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin = new stdClass();
 $plugin->component = 'mod_videoplayer';
-$plugin->version = 2026062206;
-$plugin->release = '1.1.7-beta';
+$plugin->version = 2026062207;
+$plugin->release = '1.1.8-beta';
 $plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_BETA;
