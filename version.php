9ace0a4fd626a13e6c5d02ee09f1a1eadaeb9f52
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-06-22 08:37:37 -0600

    Release PDF.js only viewer

--- version.php
+++ version.php
@@ -3,7 +3,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin = new stdClass();
 $plugin->component = 'mod_videoplayer';
-$plugin->version = 2026062204;
-$plugin->release = '1.1.5-beta';
+$plugin->version = 2026062205;
+$plugin->release = '1.1.6-beta';
 $plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_BETA;
