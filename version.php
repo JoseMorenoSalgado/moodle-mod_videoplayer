ce72f1dd983a6ea5f02c35afb3a30bf57dd326ab
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-06-22 08:23:19 -0600

    Update ebook performance release

--- version.php
+++ version.php
@@ -3,7 +3,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin = new stdClass();
 $plugin->component = 'mod_videoplayer';
-$plugin->version = 2026062203;
-$plugin->release = '1.1.4-beta';
+$plugin->version = 2026062204;
+$plugin->release = '1.1.5-beta';
 $plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_BETA;
