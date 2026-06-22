8f9683c0fcaa99d1881f03c584b0b52bfa521171
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-06-22 02:53:06 -0600

    Bump plugin version to 1.1.3 beta

--- version.php
+++ version.php
@@ -3,7 +3,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin = new stdClass();
 $plugin->component = 'mod_videoplayer';
-$plugin->version = 2026062201;
-$plugin->release = '1.1.2-beta';
+$plugin->version = 2026062202;
+$plugin->release = '1.1.3-beta';
 $plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_BETA;
