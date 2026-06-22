693e5581a01d8e77033429684578cbf1fc20b18a
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-06-22 09:44:24 -0600

    Bump release for PDF zoom controls

--- version.php
+++ version.php
@@ -3,7 +3,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin = new stdClass();
 $plugin->component = 'mod_videoplayer';
-$plugin->version = 2026062207;
-$plugin->release = '1.1.8-beta';
+$plugin->version = 2026062208;
+$plugin->release = '1.1.9-beta';
 $plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_BETA;
