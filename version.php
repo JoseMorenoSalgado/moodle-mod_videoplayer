01e94b5370a9e51a637c492903d3412f561fc9c2
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-06-23 09:36:44 -0600

    Bump version for AMD book viewer build

--- version.php
+++ version.php
@@ -3,7 +3,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin = new stdClass();
 $plugin->component = 'mod_videoplayer';
-$plugin->version = 2026062310;
-$plugin->release = '1.1.11-beta';
+$plugin->version = 2026062311;
+$plugin->release = '1.1.12-beta';
 $plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_BETA;
