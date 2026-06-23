6f224d11c0634edff45160fc5a9373b4f168c99d
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-06-23 09:22:54 -0600

    Bump plugin version for book viewer update

--- version.php
+++ version.php
@@ -3,7 +3,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin = new stdClass();
 $plugin->component = 'mod_videoplayer';
-$plugin->version = 2026062209;
-$plugin->release = '1.1.10-beta';
+$plugin->version = 2026062310;
+$plugin->release = '1.1.11-beta';
 $plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_BETA;
