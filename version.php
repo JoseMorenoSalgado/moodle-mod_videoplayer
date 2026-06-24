17efd30027224fa07eee5a332d51514ea35d2f04
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-06-24 11:32:57 -0600

    Bump plugin version for security hardening

--- version.php
+++ version.php
@@ -3,7 +3,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin = new stdClass();
 $plugin->component = 'mod_videoplayer';
-$plugin->version = 2026062411;
-$plugin->release = '1.1.15-beta';
+$plugin->version = 2026062412;
+$plugin->release = '1.1.16-beta';
 $plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_BETA;
