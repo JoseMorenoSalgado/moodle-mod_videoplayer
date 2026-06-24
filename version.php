bd31f1657476192d842e34bf9897248137bcd62c
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-06-24 10:44:11 -0600

    Bump plugin version after protected stream refactor

--- version.php
+++ version.php
@@ -3,7 +3,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin = new stdClass();
 $plugin->component = 'mod_videoplayer';
-$plugin->version = 2026062410;
-$plugin->release = '1.1.14-beta';
+$plugin->version = 2026062411;
+$plugin->release = '1.1.15-beta';
 $plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_BETA;
