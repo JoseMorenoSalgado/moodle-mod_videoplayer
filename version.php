efaaef45a29c44fe5cfeaeaec31e16862d9084d2
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-07-17 22:16:33 -0600

    Bump version for precise media progress schema

--- version.php
+++ version.php
@@ -3,7 +3,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin = new stdClass();
 $plugin->component = 'mod_videoplayer';
-$plugin->version = 2026071701;
+$plugin->version = 2026071702;
 $plugin->release = '1.1.18-beta';
 $plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_BETA;
