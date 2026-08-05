2796723a1e865cfcb4ea398f1099a60ac4761e61
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-08-05 10:07:32 -0600

    release: bump Moodle 5.0 schema compatibility hotfix

--- version.php
+++ version.php
@@ -3,8 +3,8 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin = new stdClass();
 $plugin->component = 'mod_videoplayer';
-$plugin->version = 2026080500;
-$plugin->release = '1.1.21-beta';
+$plugin->version = 2026080501;
+$plugin->release = '1.1.22-beta';
 $plugin->requires = 2025041400;
 $plugin->supported = [500, 502];
 $plugin->maturity = MATURITY_BETA;
