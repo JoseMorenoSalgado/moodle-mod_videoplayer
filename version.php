0e01a22293575815eb87efbcc3dd54fef89525db
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-08-05 09:51:42 -0600

    release: declare Moodle 5.0 compatibility

--- version.php
+++ version.php
@@ -3,7 +3,8 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin = new stdClass();
 $plugin->component = 'mod_videoplayer';
-$plugin->version = 2026072401;
-$plugin->release = '1.1.20-beta';
+$plugin->version = 2026080500;
+$plugin->release = '1.1.21-beta';
 $plugin->requires = 2025041400;
+$plugin->supported = [500, 502];
 $plugin->maturity = MATURITY_BETA;
