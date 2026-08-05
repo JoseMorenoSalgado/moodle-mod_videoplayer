8af44849a64ce975f78757d16fdbc8755e48f5bb
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-08-05 12:32:33 -0600

    release: bump Drive Resource to 1.1.23-beta

--- version.php
+++ version.php
@@ -26,8 +26,8 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin = new stdClass();
 $plugin->component = 'mod_videoplayer';
-$plugin->version = 2026080501;
-$plugin->release = '1.1.22-beta';
+$plugin->version = 2026080502;
+$plugin->release = '1.1.23-beta';
 $plugin->requires = 2025041400;
 $plugin->supported = [500, 502];
 $plugin->maturity = MATURITY_BETA;
